57d167e8aea8e41a0c040685eb9678bc70f85b76
lcczk <s2276294@ed.ac.uk>
2026-06-03 19:16:13 +0100

    videos and utils

---
+++ src/humanoid_swe_challenge/mcp/utils.py
@@ -0,0 +1,18 @@
+import numpy as np
+import base64
+from PIL import Image
+import io
+
+def obs_to_dict(obs: dict) -> dict:
+    obs_c = obs.copy()
+    for  k, v in obs_c.items():
+        if isinstance(v, np.ndarray):
+            obs_c[k]=v.tolist()
+            
+    return obs_c
+
+def frame_to_base64(frame: np.ndarray, format: str = "JPEG", quality: int = 85) -> str:
+    img = Image.fromarray(frame)
+    buffer = io.BytesIO()
+    img.save(buffer, format=format, quality=quality if format == "JPEG" else None)
+    return base64.b64encode(buffer.getvalue()).decode("utf-8")
